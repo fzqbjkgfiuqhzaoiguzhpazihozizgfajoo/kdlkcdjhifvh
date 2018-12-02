2ccac856a260f2d18dbb3ac31159863da4d59695
fzqbjkgfiuqhzaoiguzhpazihozizgfajoo <45123656+fzqbjkgfiuqhzaoiguzhpazihozizgfajoo@users.noreply.github.com>
2018-12-02 17:19:01 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -229,21 +229,7 @@ function play(guild, song) {
 }
 
 
-client.on('message', message => {
-    if (message.content === 'help') {
-        let helpEmbed = new Discord.RichEmbed()
-        .setTitle('**أوامر الميوزك...**')
-        .setDescription('**برفكس البوت (!)**')
-        .addField('play', 'لتشغيل اغنية')
-        .addField('join', 'دخول رومك الصوتي')
-        .addField('disconnect', 'الخروج من رومك الصوتي')
-        .addField('skip', 'تخطي الأغنية')
-        .addField('pause', 'ايقاف الاغنية مؤقتا')
-        .addField('resume', 'تكملة الاغنية')
-        .addField('queue', 'اظهار قائمة التشغيل')
-        .addField('np', 'اظهار الاغنية اللي انت مشغلها حاليا')
-      message.channel.send(helpEmbed);
-    }
-});
+client.on("error", (e)=> console.log(e)); 
+
 
 client.login(process.env.BOT_TOKEN);
